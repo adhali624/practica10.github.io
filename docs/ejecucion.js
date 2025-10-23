ddfad5c0ac2fc667a5a136cb46bf83406d294d83
adhali624 <lopezmotakerenadhaligen2023@cbtis116.edu.mx>
2025-10-22 21:38:40 -0700

    Update ejecucion.js

--- docs/ejecucion.js
+++ docs/ejecucion.js
@@ -1,14 +1,20 @@
+// Tomamos los elementos del HTML
 const imagen = document.getElementById("imagen");
 const panel = document.getElementById("panel");
 const cerrarBtn = document.getElementById("cerrarBtn");
 
+// Función para mostrar el panel
 function mostrarReceta() {
   panel.style.display = "flex";
 }
 
+// Función para cerrar el panel
 function cerrarReceta() {
   panel.style.display = "none";
 }
 
+// Cuando se da clic en la imagen, muestra el panel
 imagen.addEventListener("click", mostrarReceta);
+
+// Cuando se da clic en el botón, cierra el panel
 cerrarBtn.addEventListener("click", cerrarReceta);
